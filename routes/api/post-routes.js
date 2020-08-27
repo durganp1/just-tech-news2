4a0402c9aefb5a7a90f46700647ea3d80a36fb71
durganp1 <durganp1@gmail.com>
2020-08-27 14:07:50 -0500

    add vote count to post route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,17 @@
 
 
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
+
 
 router.get('/', (req, res) => {
     console.log('=============');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+        [sequelize.literal(`(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`),
+        'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -27,7 +32,10 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+        [sequelize.literal(`(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`),
+        'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -58,6 +66,29 @@ router.post('/', (req, res) => {
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
+    });
+});
+
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(() => {
+        return Post.findOne({
+            where: {
+                id: req.body.post_id
+            },
+            attributes: ['id', 'post_url', 'title', 'created_at', 
+            [sequelize.literal(`(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`),
+            'vote_count']
+            ]
+        })
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
     });
 });
 
