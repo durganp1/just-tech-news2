ebccb8d0f9040c10bf61669f2b1c874b02b5f0f3
durganp1 <durganp1@gmail.com>
2020-08-26 20:42:16 -0500

    add bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 
 
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -31,9 +32,19 @@ User.init(
             validate: {
                 len: [4]
             }
-        }
+        },
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
