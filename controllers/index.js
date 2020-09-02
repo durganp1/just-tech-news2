61e056db1c1d39076e0fb4548d4c87975ee4e118
durganp1 <durganp1@gmail.com>
2020-09-02 17:37:38 -0500

    add homepage with handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 
 
+const homeRoutes = require('./home-routes');
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
 router.use((req, res) => {
     res.status(404).end();
 });
