c63fe533c7db52fb40d72d42c9ea778d3288de09
durganp1 <durganp1@gmail.com>
2020-09-03 14:00:32 -0500

    add cookies and logout

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 
 
+require('dotenv').config();
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: (process.env.ES_SESS),
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
